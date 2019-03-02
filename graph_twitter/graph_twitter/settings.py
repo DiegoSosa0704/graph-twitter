17897dcfe1e5a32ee739b6b8bcf9858630f5175f
Ubuntu <ubuntu@ip-172-31-82-242.ec2.internal>
2019-03-02 15:08:45 +0000

    Commit server

--- graph_twitter/graph_twitter/settings.py
+++ graph_twitter/graph_twitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't5sajyx^cb#*c9rqwgrml(frit6lrn@zuet_mq=^__h#kze*ix'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.156.11.86']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Credentials AWS
 ACCESS_ID = 'AKIAJRIW35C6SG2PIQWQ'
