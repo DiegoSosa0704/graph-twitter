618e223914a530c7fab01b5927623e0e1ba1ae9f
Diego Sosa <diego.sosa0704@gmail.com>
2019-03-05 12:24:34 -0500

    Agrego funciones para gráficar sentimientos de tweets.

--- graph_twitter/graph_twitter/settings.py
+++ graph_twitter/graph_twitter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't5sajyx^cb#*c9rqwgrml(frit6lrn@zuet_mq=^__h#kze*ix'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['54.156.11.86']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
