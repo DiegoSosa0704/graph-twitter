260bee923f7a97b05e67c112d5ab492001b87c99
Diego Sosa <diego.sosa0704@gmail.com>
2019-03-06 13:38:07 -0500

    Agrego archivos para tareas programadas

--- graph_twitter/cronRequests.sh
+++ graph_twitter/cronRequests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source ~/graph-twitter/env/bin/activate
-0 12 * * * python3 send_request.py
+5 12 * * * send_request.py
 deactivate
--- graph_twitter/send_request.py
+++ graph_twitter/send_request.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
 import requests
+import sys
 
+args = sys.argv
 event = {
-    "keyword": "bicentenario",
+    "keyword": args[1],
     "lang": "es",
-    "count": 100,
+    "count": int(args[2]),
     "geocode": "5.533,-73.367,15km",
-    "campaign_id": "d71824f1-63d0-4ce6-9e7e-020ed5b09de8"
+    "campaign_id": args[3]
 }
 requests.post('http://10.4.44.114/api/v1/search_tweet_geocode/', data=event)
